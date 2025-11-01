3485fdf7be3a859f4d40543dd8c8e1006e106fa7
ggrrcc <56850848+ggrrcc@users.noreply.github.com>
2025-11-01 16:44:06 -0400

    Addn files

--- 1.0_OSINT_csv_generator.py
+++ 1.0_OSINT_csv_generator.py
@@ -1,9 +1,8 @@
 import pandas as pd
-# https://www.kaggle.com/datasets/piterfm/massive-missile-attacks-on-ukraine?resource=download
-# Load the dataset
+# Load the dataset ### EXAMPLE DATASET
 OSINT_df = pd.read_csv('Data/missile_attacks_daily.csv')  # Source: https://www.kaggle.com/datasets/piterfm/massive-missile-attacks-on-ukraine?resource=download
 
-# Clean blanks and NaNs
+# Clean blanks and NaNs- but this info may be useful @ a later time TODO: Analysis
 OSINT_df['time_start'] = OSINT_df['time_start'].astype(str).str.strip()
 OSINT_df['time_end'] = OSINT_df['time_end'].astype(str).str.strip()
 
@@ -11,11 +10,11 @@ OSINT_df['time_end'] = OSINT_df['time_end'].astype(str).str.strip()
 OSINT_df = OSINT_df[(OSINT_df['time_start'].str.len() > 10) & (OSINT_df['time_end'].str.len() > 10)]
 OSINT_df.dropna(subset=['time_start', 'time_end', 'target'], inplace=True)
 
-# Convert to datetime safely
+# Convert to datetime
 OSINT_df['time_start'] = pd.to_datetime(OSINT_df['time_start'], errors='coerce')
 OSINT_df['time_end'] = pd.to_datetime(OSINT_df['time_end'], errors='coerce')
 
-# Drop rows where datetime conversion failed
+# Drop rows where conversion failed
 OSINT_df.dropna(subset=['time_start', 'time_end'], inplace=True)
 
 # Define March boundaries
@@ -25,7 +24,7 @@ march_end = pd.Timestamp('2025-03-31 23:59')
 # Filter within March boundaries
 df_filtered = OSINT_df[(OSINT_df['time_start'] >= march_start) & (OSINT_df['time_end'] <= march_end)]
 
-# Save the filtered DataFrame
-OSINT_df[['time_start', 'time_end', 'target']].to_csv('OSINT.csv', index=False)
+# Save the filtered DataFrame, should be in order
+OSINT_df[['time_start', 'time_end', 'target']].to_csv('Data/cleaned_attacks.csv', index=False)
 
-print("Filtered dataset saved to 'Data/OSINT.csv'")
+print("Filtered dataset saved to 'Data/cleaned_attacks.csv'")
--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Blockchain compliance
 A partial look into my work on this project with proprietary parts removed.
  
+This project was Project #4: Blockchain Compliance & Crypto Sanction Evasion Detection at Lehigh University, sponsored by J.S. Held, Inca Digital, DARPA, and the head of trading at Norrio Capital.
